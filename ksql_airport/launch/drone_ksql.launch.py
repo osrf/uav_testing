e601ced9a59fc1f518f668b49a40cb010db200cb
ahcorde <ahcorde@gmail.com>
2019-10-23 17:07:31 +0200

    Remove commented python imports

--- ksql_airport/launch/drone_ksql.launch.py
+++ ksql_airport/launch/drone_ksql.launch.py
@@ -12,14 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# from launch import LaunchDescription
-# from launch.actions import DeclareLaunchArgument
-# from launch.actions import IncludeLaunchDescription
-# from launch.conditions import IfCondition
-# from launch.launch_description_sources import PythonLaunchDescriptionSource
-# from launch.substitutions import LaunchConfiguration
-# from launch.substitutions import ThisLaunchFileDir
-
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
