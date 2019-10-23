61b468937e395e407ca8b43d9168edd27bca5af3
ahcorde <ahcorde@gmail.com>
2019-10-23 16:41:59 +0200

    Adding yosemite ROS 2 launch files

---
+++ yosemite_valley/launch/yosemite.launch.py
@@ -0,0 +1,39 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# from launch import LaunchDescription
+# from launch.actions import DeclareLaunchArgument
+# from launch.actions import IncludeLaunchDescription
+# from launch.conditions import IfCondition
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.substitutions import LaunchConfiguration
+# from launch.substitutions import ThisLaunchFileDir
+
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    gazebo_dir = get_package_share_directory('gazebo_ros')
+    included_launch = launch.actions.IncludeLaunchDescription(
+                launch.launch_description_sources.PythonLaunchDescriptionSource(gazebo_dir + '/launch/gazebo.launch.py'),
+                launch_arguments={'world': 'worlds/yosemite.world',
+                                  'paused': 'false',
+                                  'physics': 'ode'}.items()
+                )
+    return LaunchDescription([
+        included_launch,
+    ])
